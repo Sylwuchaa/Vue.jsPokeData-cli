355839a8a84a6a74a58fe3b084eab4fd7ff9225e
Sylwcuhaa <uzumagi1@gmail.com>
2018-07-15 19:45:04 +0200

    added axios library, axios get response, favicon

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,14 +5,36 @@
     </div>
     <div class="search">
     <label for="search">Search</label>
-    <input id="search" name="search" />
+    <input id="search"
+    name="search"
+    v-model="searchValue" 
+    @input="handleInput"
+    />
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios';
+
+const API = 'http://pokeapi.co/media/sprites/pokemon/';
 export default {
   name: 'Search',
-  components: {},
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+  methods: {
+    handleInput() {
+      axios.get(`${API}${this.searchValue}.png`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => { 
+        console.log(error);
+      });
+    },
+  },
 };
 </script>
 
@@ -24,25 +46,34 @@ export default {
     font-size: 10px;
     font-family: Lato, sans-serif;
     color: #ffffff;
+    position: relative;
+    width: 100%;
+    height: 100%; 
   }
-.wrapper {
-  background-size: no-repeat;
-  background-image: linear-gradient(to right, #09052f 0%, #311d5e 100%);
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  margin: 0;
-  padding: 0;
-  height: 91.6vh;
-}
-.search {
-  text-align: center;
-  color: #13102d;
-  width: 35rem;
-  display: flex;
-  flex-direction: column;
-  width: 18rem;
+  html {
+    font-size: 10px;
+    position: unset;
+    width: 100%;
+    height: 100%; 
+  }
+  .wrapper {
+    background-size: no-repeat;
+    background-image: linear-gradient(to right, #09052f 0%, #311d5e 100%);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    // height: 91.6vh;
+  }
+  .search {
+    text-align: center;
+    color: #13102d;
+    width: 35rem;
+    display: flex;
+    flex-direction: column;
+    width: 18rem;
 
   label {
     padding-bottom: 1.5rem;
